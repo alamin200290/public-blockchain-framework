175d5818cfbfc7ee5d2e677c31a5b55e3f4d7b54
sarwar_sikder <amin.se.aiub@gmail.com>
2020-10-24 23:46:10 +0600

    view engine added

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+
+//configuration
+app.set('view engine', 'ejs');
 
 
-
+//routing
 app.get('/', (request, response)=>{
 	response.send('welcome! Explore your blockchain knowledge with our custom made public blockchain framework!!! ');
 })
 
 
+//server startup
 app.listen(3000, ()=>{
 	"server started at ...3000";
 });
